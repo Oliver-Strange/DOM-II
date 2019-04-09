e40528be3ca09c75bcba4496c011058c013ed190
Oliver-Strange <preston.burton@gmail.com>
2019-04-09 13:04:07 -0700

    added first event listener to the nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+const navEvent = document.querySelector('.nav');
+
+navEvent.addEventListener('mouseover', event =>{
+    event.target.style.color = 'blue';
+    event.target.style.fontSize = '2.5rem';
+    setTimeout(function(){
+        event.target.style.color = '';
+        event.target.style.fontSize = '';
+    }, 250);
+}, false);
+
+/*
+.addEventListener('keydown', function(event){
+
+});
+.addEventListener('wheel', function(event){
+
+});
+.addEventListener('load', function(event){
+
+});
+.addEventListener('focus', function(event){
+
+});
+.addEventListener('resize', function(event){
+
+});
+.addEventListener('scroll', function(event){
+
+});
+.addEventListener('select', function(event){
+
+});.addEventListener('dbclick', function(event){
+
+});
+.addEventListener('drag', function(event){
+
+});
+*/
